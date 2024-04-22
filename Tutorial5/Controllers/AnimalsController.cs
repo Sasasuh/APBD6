f61edf71f5474d997cd048d53bb9e8092f23b7b1
Sasasuh <oleksandrhashev201@gmail.com>
2024-04-22 21:38:15 +0200

    Init proj

---
+++ Tutorial5/Controllers/AnimalsController.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Tutorial5.Models;
+using Tutorial5.Models.DTOs;
+
+namespace Tutorial5.Controllers;
+
+[ApiController]
+// [Route("api/animals")]
+[Route("api/[controller]")]
+public class AnimalsController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+
+    public AnimalsController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    [HttpGet]
+    public IActionResult GetAnimals([FromQuery] string orderBy = "name")
+    {
+        string orderByClause = "";
+        switch (orderBy.ToLower())
+        {
+            case "name":
+                orderByClause = "Name";
+                break;
+            case "description":
+                orderByClause = "Description";
+                break;
+            case "category":
+                orderByClause = "Category";
+                break;
+            case "area":
+                orderByClause = "Area";
+                break;
+            default:
+                return BadRequest("Invalid orderBy argument. Available options: Name, Description, Category, Area");
+        }
+
+
+        // Uruchamiamy połączenie do bazy
+        using SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Default"));
+        connection.Open();
+
+        // Definiujemy command
+        using SqlCommand command = new SqlCommand();
+        command.Connection = connection;
+        command.CommandText = $"SELECT * FROM Animal ORDER BY {orderByClause}";
+
+        // Uruchomienie zapytania
+        var reader = command.ExecuteReader();
+
+        List<Animal> animals = new List<Animal>();
+
+        int idAnimalOrdinal = reader.GetOrdinal("IdAnimal");
+        int nameOrdinal = reader.GetOrdinal("Name");
+        int descriptionOrdinal = reader.GetOrdinal("Description");
+        int categoryOrdinal = reader.GetOrdinal("Category");
+        int areaOrdinal = reader.GetOrdinal("Area");
+
+        while (reader.Read())
+        {
+            animals.Add(new Animal()
+            {
+                IdAnimal = reader.GetInt32(idAnimalOrdinal),
+                Name = reader.GetString(nameOrdinal),
+                Description = reader.GetString(descriptionOrdinal),
+                Category = reader.GetString(categoryOrdinal),
+                Area = reader.GetString(areaOrdinal)
+            });
+        }
+
+        //var animals = _repository.GetAnimals();
+
+        return Ok(animals);
+    }
+
+
+    [HttpPost]
+    public IActionResult AddAnimal(AddAnimal addAnimal)
+    {
+        // Uruchamiamy połączenie do bazy
+        using SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Default"));
+        connection.Open();
+
+        // Definiujemy command
+        using SqlCommand command = new SqlCommand();
+        command.Connection = connection;
+
+        // Stworzylem tabele z slowem kluczowym IDENTITY(1,1), wiec IdAnimal jest dodawany automatycznie przez sql server.
+        //Np. kiedy chce wpisac id, to moge wylaczyc IDENTITY za pomoca: SET IDENTITY_INSERT Animal ON;
+        command.CommandText =
+            "INSERT INTO Animal(Name, Description, Category, Area) VALUES(@animalName,@animalDescription,@amimalCategory,@animalArea)";
+        command.Parameters.AddWithValue("@animalName", addAnimal.Name);
+        command.Parameters.AddWithValue("@animalDescription", addAnimal.Description);
+        command.Parameters.AddWithValue("@animalCategory", addAnimal.Category);
+        command.Parameters.AddWithValue("@animalArea", addAnimal.Area);
+
+        // Wykonanie commanda
+        command.ExecuteNonQuery();
+
+        //_repository.AddAnimal(addAnimal);
+
+        return Created("", null);
+    }
+
+    [HttpPut("{idAnimal}")]
+    public IActionResult UpdateAnimal(int idAnimal, AddAnimal addAnimal)
+    {
+        using SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Default"));
+        connection.Open();
+
+        using SqlCommand command = new SqlCommand();
+        command.Connection = connection;
+
+        //Declaring command
+        command.CommandText =
+            "UPDATE Animal SET Name = @animalName, Description = @animalDescription, Category = @animalCategory, Area = @animalArea WHERE IdAnimal = @idAnimal";
+        command.Parameters.AddWithValue("@idAnimal", idAnimal);
+        command.Parameters.AddWithValue("@animalName", addAnimal.Name);
+        command.Parameters.AddWithValue("@animalDescription", addAnimal.Description);
+        command.Parameters.AddWithValue("@animalCategory", addAnimal.Category);
+        command.Parameters.AddWithValue("@animalArea", addAnimal.Area);
+
+
+        int rowsAffected = command.ExecuteNonQuery();
+        if (rowsAffected == 1)
+        {
+            return NoContent();
+        }
+        else
+        {
+            return StatusCode(500, "Failed to update animal");
+        }
+    }
+
+    [HttpDelete("{idAnimal}")]
+    public IActionResult DeleteAnimal(int idAnimal)
+    {
+        using SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Default"));
+        connection.Open();
+
+        using SqlCommand command = new SqlCommand();
+        command.Connection = connection;
+
+        //Declaring command
+        command.CommandText = "DELETE FROM Animal WHERE IdAnimal = @idAnimal ";
+        command.Parameters.AddWithValue("@idAnimal", idAnimal);
+
+
+        int rowsAffected = command.ExecuteNonQuery();
+        if (rowsAffected == 1)
+        {
+            return NoContent();
+        }
+        else
+        {
+            return NotFound("Animal with that ID not found");
+        }
+    }
+}
